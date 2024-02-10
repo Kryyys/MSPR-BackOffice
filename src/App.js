1c3b964d95ddf6d02c0c25a679fec451773dce28
Marine C <mariine.becker@gmail.com>
2024-02-10 10:31:33 +0100

    signup page and login ok

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from "./views/list/List";
 import Offers from "./views/offers/Offers";
 import SeeUser from "./views/seeUser/SeeUser";
 import SeeOffer from "./views/seeOffer/SeeOffer";
+import SignUp from "./views/signUp/SignUp";
 import {
   BrowserRouter,
   Routes,
@@ -18,6 +19,7 @@ function App() {
         <Route path="/">
           <Route index element={<Home/>}/>
           <Route path="login" element={<Login/>}/>
+          <Route path="signup" element={<SignUp/>}/>
           <Route path="offers" element={<Offers/>}/>
           <Route path="users">
             <Route index element={<List/>}/>
